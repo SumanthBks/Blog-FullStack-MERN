1f532ee7275200b6ed268ac59cb81530c0f0a980
Sumanth <52619973+Sumanth-web@users.noreply.github.com>
2020-07-09 12:03:18 +0530

    Blog-First Commit

---
+++ src/build/precache-manifest.9570a661f14cc3e40339326514bb2b27.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1d2f1a73a54abbc8dea13ba37f6f4322",
+    "url": "/index.html"
+  },
+  {
+    "revision": "011c93ff040b398c1a51",
+    "url": "/static/css/main.fe6824ef.chunk.css"
+  },
+  {
+    "revision": "77e1fa18b2723bba68e0",
+    "url": "/static/js/2.ac4ccbdb.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.ac4ccbdb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "011c93ff040b398c1a51",
+    "url": "/static/js/main.3958c90f.chunk.js"
+  },
+  {
+    "revision": "103618288f3bfdd689ae",
+    "url": "/static/js/runtime-main.7acdd037.js"
+  }
+]);
